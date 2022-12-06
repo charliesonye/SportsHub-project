e9e422dc1abbfc8d72004e25f21a322084c3551f
Charles <c_onyemem@live.com>
2022-12-05 16:51:05 -0800

    Resources for User has been limited to two get and post routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,18 @@ class UsersController < ApplicationController
 
     def create
         user = User.create(user_params)
+        session[:user_id] = user.id
         render json: user
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :created
+        else
+            render json: {error:  'Not Authorized'}, status: :unauthorized
+        end
+    
     end
 
     private
